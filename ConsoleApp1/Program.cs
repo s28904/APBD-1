069e8e1659f2c3ae4386e948a1939478c8ff1b9b
s28904 <s28904@pjwstk.edu.pl>
2024-03-08 12:08:36 +0100

    GetMax function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(getAvg([1,2,3,4,5,5]));
-
+Console.WriteLine(GetMax([1, 2, 3, 4, 7, 5]));
 return;
+
+static int GetMax(int[] arr)
+{
+    int max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }    
+    }    return max;
+}
 
 static double getAvg(int[] arr)
 {
