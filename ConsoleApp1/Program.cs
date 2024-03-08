2e7ae746f2ce2bbc58342bc6bb221532f9493095
s28904 <s28904@pjwstk.edu.pl>
2024-03-08 11:51:49 +0100

    average-counter

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(getAvg([1,2,3,4,5,5]));
+
+return;
+
+static double getAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+    return (float)sum / arr.Length;
+}
