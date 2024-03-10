d83ab32a4bd517c694924b31214b64b90bd48671
s28904 <s28904@pjwstk.edu.pl>
2024-03-10 14:36:16 +0100

    ftr-new variable change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,10 +17,10 @@ static int GetMax(int[] arr)
 
 static double getAvg(int[] arr)
 {
-    int sumZmieniona = 0;
+    int sumZmianaV2 = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        sum += arr[i];
+        sumZmianaV2 += arr[i];
     }
     return (float)sum / arr.Length;
 }
